55f74af8cc9a9f15dd22febbfede07b8135acd39
David Avellaneda <g.avella@gmail.com>
2015-10-10 14:42:00 -0500

    adding footer and basic navigation to jsconfco

---
+++ client/footer.jsx
@@ -0,0 +1,11 @@
+Footer = React.createClass({
+  render() {
+    return <footer className="main-footer">
+      <div className="container">
+          <h3><a href="http://jsconf.co">JSConf Colombia</a></h3>
+          <p>JSConf Colombia is <strong>non-profit</strong> community conference organized by <strong>volunteers</strong> of the ColJS community. <br/>ColJS is a "passion driven organization" working to promote JavaScript and its community grow in Colombia.</p>
+          <p>We adopt and enforce the <a href="/code-of-conduct/">JSConf Code of Conduct</a> for all of our community events.</p>
+      </div>
+    </footer>
+  }
+})
